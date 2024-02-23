dfcdd4e603bc0e81bd494e7fc6f086193cfc0a10
André Wakugawa <117671087+AndreWakugawa@users.noreply.github.com>
2024-02-23 20:21:35 -0300

    Add files via upload

---
+++ Lab3/FirstServer/src/main/java/com/andrewakugawa/firstserver/FirstserverApplication.java
@@ -0,0 +1,13 @@
+package com.andrewakugawa.firstserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstserverApplication.class, args);
+	}
+
+}
---
+++ Lab3/FirstServer/src/test/java/com/andrewakugawa/firstserver/FirstserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewakugawa.firstserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
